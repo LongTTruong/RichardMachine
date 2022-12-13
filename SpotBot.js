ce2ae152bd9aee80429e29508d59e98f7d9678fe
Long <longtruong31@gmail.com>
2022-12-13 14:17:48 -0500

    More lines

--- SpotBot.js
+++ SpotBot.js
@@ -1,29 +1,39 @@
 const eris = require('eris');
-const prefix = '!sb';
+const PREFIX = '!sb ';
 const {bot_token} = require('./config.json');
 const bot = new eris.Client(bot_token);
+const commandHandlerForCommandName = {};
+
+commandHandlerForCommandName['play'] = (args) => {
+    const song = args[0];
+    const channel = args[1];
+    return msg.channel.createMessage('Now Playing: ${song} in $${channel}.')
+};
 
 bot.on('ready', () => {
     console.log('Connected and ready.');
 });
 
 bot.on('messageCreate', async (msg)=> {
-    const botWasMention = msg.mentions.find(
-        mentionedUser => mentionedUser.id === bot.user.id,
-    );
+    const content = msg.content;
 
-    if (botWasMention) {
-        try {
-            await msg.channel.createMessage("I'm over here stroking my dick, got lotion on my dick, I'm strokin my shit.")
-        } catch (err){
-            console.warn('Failed to respond.');
-            console.warn(err)
-        }
+    if (!content.startsWith(PREFIX)){
+        return;
     }
+
+    const parts = 
+    const commandName = ;
+
+
+    const commandHandler = commandHandlerForCommandName[commandName];
+
+    if (!commandHandler) {
+        return;
+    }
+
 });
 
 bot.on('error', err=> {
     console.warn(err)
 });
-
 bot.connect();
